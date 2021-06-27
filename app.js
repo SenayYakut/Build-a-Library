8d2936fd36a3c6436c3d914c3a7855ce7eeaed8f
Senay Yakut <senaykt@gmail.com>
2021-06-26 20:52:48 -0700

    Add inherited class

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 class Media {
-    constructor(keyOne) {
+    constructor(title) {
         this._title = title;
-        this._isCheckedOut = isCheckedOut;
-        this._ratings = ratings;
-
+        this._isCheckedOut = false;
+        this._ratings = [];
     }
     get title() {
         return this._title;
@@ -14,37 +13,65 @@ class Media {
     get ratings() {
         return this._ratings;
     }
+    set isCheckedOut(value) {
+        this._isCheckedOut = value;
+    }
     toggleCheckOutStatus() {
         this._isCheckedOut = !this._isCheckedOut;
     }
     getAverageRating() {
-        let ratingsSum = this.ratings.reduce((currentSum, rating) => currentSum + rating, 0);
-        const lengthOfArray = ratings.length;
-        return ratingsSum / lengthOfArray;
+        let ratingsSum = this.ratings.reduce((accumulator, rating) => accumulator + rating);
+        return ratingsSum / this.ratings.length;
     }
-    set isCheckedOut(newKey) {
-        this._isCheckedOut = newKey;
+
+    addRating(value) {
+        this._ratings.push(value);
     }
 }
 
 class Book extends Media {
-    constructor(author, ) {
-        constructor(author, isCheckedOut, pages) {
-            this._author = author;
-            this._isCheckedOut = false;
-            this._pages = pages;
+    constructor(author, title, pages) {
+        super(title);
+        this._author = author;
+        this._pages = pages;
 
-        }
-        get author() {
-            return this._author;
-        }
-        get getAverageRating() {
-
-        }
-        get toggleCheckOutStatus() {
-
-        }
-        get addRating() {
-
-        }
     }
+    get author() {
+        return this._author;
+    }
+    get pages() {
+        return this._pages;
+    }
+}
+
+
+class Movie extends Media {
+    constructor(director, title, runTime) {
+        super(title);
+        this._director = director;
+        this._runTime = runTime;
+    }
+    get director() {
+        return this._director;
+    }
+    get runTime() {
+        return this._runTime;
+    }
+}
+
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+
+historyOfEverything.toggleCheckOutStatus();
+console.log(historyOfEverything.isCheckedOut);
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+console.log(historyOfEverything.getAverageRating());
+
+const speed = new Movie('Jan de Bont', 'Speed', 116);
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+console.log(speed.getAverageRating());
